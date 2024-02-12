3b538f094bf60a009b99cf00d80a209deb25b8c5
Rasaq Ganiyu <eudoxusganium@gmail.com>
2024-02-12 16:41:30 +0100

    restyling product list page

--- src/components/productList.jsx
+++ src/components/productList.jsx
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import {fetchProduct} from '@/services';
 import Link from 'next/link';
+import {Loader} from './assets/icons';
 
 const ProductList = () => {
   const [products, setProducts] = useState([]);
@@ -21,33 +22,43 @@ const ProductList = () => {
         </h2>
 
         <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-          {products?.map(product => (
-            <Link key={product.id} href={`single-product-page/${product.id}`}>
-              <div key={product.id} className="group relative cursor-pointer">
-                <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
-                  <img
-                    src={product.imageLocation}
-                    alt={product.name}
-                    className="h-full w-full object-contain object-center lg:h-full lg:w-full"
-                  />
-                </div>
-                <div className="mt-4 flex justify-between">
-                  <div>
-                    <h3 className="text-sm text-gray-700">
-                      <a href={product.href}>
-                        <span aria-hidden="true" className="absolute inset-0" />
-                        {product.name}
-                      </a>
-                    </h3>
-                    {/* <p className="mt-1 text-sm text-gray-500">{product.color}</p> */}
+          {products.length > 0 ? (
+            products?.map(product => (
+              <Link key={product.id} href={`single-product-page/${product.id}`}>
+                <div
+                  key={product.id}
+                  className="group relative cursor-pointer shadow-md p-4 rounded-sm">
+                  <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
+                    <img
+                      src={product.imageLocation}
+                      alt={product.name}
+                      className="h-full w-full object-contain object-center lg:h-full lg:w-full"
+                    />
                   </div>
-                  <p className="text-sm font-medium text-gray-900">
-                    {product.currencySymbol} {product.price}
-                  </p>
+                  <div className="mt-4 flex justify-between">
+                    <div>
+                      <h3 className="text-sm text-gray-700">
+                        <a href={product.href}>
+                          <span
+                            aria-hidden="true"
+                            className="absolute inset-0"
+                          />
+                          {product.name}
+                        </a>
+                      </h3>
+                    </div>
+                    <p className="text-sm font-medium text-gray-900">
+                      {product.currencySymbol} {product.price}
+                    </p>
+                  </div>
                 </div>
-              </div>
-            </Link>
-          ))}
+              </Link>
+            ))
+          ) : (
+            <div className="h-[400px] w-screen flex justify-center items-center">
+              <Loader />
+            </div>
+          )}
         </div>
       </div>
     </div>
--- src/pages/create-product-page/index.js
+++ src/pages/create-product-page/index.js
@@ -185,9 +185,9 @@ const CreateProduct = () => {
         </form>
       </div>
 
-      <div className="p-6 border-t border-gray-200 rounded-b">
+      <div className="p-6 border-t border-gray-200 rounded-b flex justify-end">
         <button
-          className="text-white bg-cyan-600 hover:bg-cyan-700 focus:ring-4 focus:ring-cyan-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center cursor-pointer"
+          className="text-white w-[300px]  bg-cyan-600 hover:bg-cyan-700 focus:ring-4 focus:ring-cyan-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center cursor-pointer "
           type="submit"
           onClick={handleSubmit}>
           Add
